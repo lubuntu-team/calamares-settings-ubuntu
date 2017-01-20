3dc5a5a004744c222a6518ada55c104cadd3e12e
Jonathan Riddell <jr@jriddell.org>
2017-01-20 10:08:25 -0500

    add restricted install

---
+++ usr/share/calamares/netinstall.php
@@ -0,0 +1,14 @@
+- name: "Restricted Media Formats"
+  description: "Media tools with restricted uses"
+  packages:
+    - flashplugin-installer
+    - gstreamer1.0-plugins-ugly
+    - gstreamer1.0-plugins-bad
+    - gstreamer1.0-libav
+    - libk3b6-extracodecs
+    - libdvdread4
+    - chromium-codecs-ffmpeg-extra
+    - oxideqt-codecs-extra
+    - unrar
+    - lame
+    - libavcodec-extra
