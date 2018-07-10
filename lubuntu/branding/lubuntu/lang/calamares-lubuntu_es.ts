185dcb48525f53bd540acacbc8e4622fab6a4e92
Simon Quigley <tsimonq2@lubuntu.me>
2018-07-10 10:07:35 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Lubuntu/Calamares Slideshow
    Translate-URL: https://translate.lubuntu.me/projects/lubuntu/calamares-slideshow/es/

--- lubuntu/branding/lubuntu/lang/calamares-lubuntu_es.ts
+++ lubuntu/branding/lubuntu/lang/calamares-lubuntu_es.ts
@@ -1,87 +1,87 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="es">
 <context>
     <name>show</name>
     <message>
         <location filename="../show.qml" line="32"/>
         <source>Get Support</source>
-        <translation type="unfinished"></translation>
+        <translation>Obtén Soporte</translation>
     </message>
     <message>
         <location filename="../show.qml" line="56"/>
         <source>https://lubuntu.me</source>
-        <translation type="unfinished"></translation>
+        <translation>https://lubuntu.me</translation>
     </message>
     <message>
         <location filename="../show.qml" line="90"/>
         <source>Your Mail</source>
-        <translation type="unfinished"></translation>
+        <translation>Tu e-mail</translation>
     </message>
     <message>
         <location filename="../show.qml" line="114"/>
         <source>Managed by Trojitá</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestionado por Trojitá</translation>
     </message>
     <message>
         <location filename="../show.qml" line="148"/>
         <source>SMPlayer</source>
-        <translation type="unfinished"></translation>
+        <translation>SMPlayer</translation>
     </message>
     <message>
         <location filename="../show.qml" line="172"/>
         <source>To Play All of Your Movies</source>
-        <translation type="unfinished"></translation>
+        <translation>Para Reproducir Todas Tus Películas</translation>
     </message>
     <message>
         <location filename="../show.qml" line="206"/>
         <source>LibreOffice</source>
-        <translation type="unfinished"></translation>
+        <translation>LibreOffice</translation>
     </message>
     <message>
         <location filename="../show.qml" line="230"/>
         <source>Edits All Of Your Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Edita Todos Tus Documentos</translation>
     </message>
     <message>
         <location filename="../show.qml" line="264"/>
         <source>Pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>Fotos</translation>
     </message>
     <message>
         <location filename="../show.qml" line="288"/>
         <source>Viewed With Nomacs</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostradas Con Nomacs</translation>
     </message>
     <message>
         <location filename="../show.qml" line="322"/>
         <source>Have Fun!</source>
-        <translation type="unfinished"></translation>
+        <translation>¡Diviértete!</translation>
     </message>
     <message>
         <location filename="../show.qml" line="346"/>
         <source>With the 2048 game</source>
-        <translation type="unfinished"></translation>
+        <translation>Con el juego 2048</translation>
     </message>
     <message>
         <location filename="../show.qml" line="380"/>
         <source>IRC Chat</source>
-        <translation type="unfinished"></translation>
+        <translation>Chat de IRC</translation>
     </message>
     <message>
         <location filename="../show.qml" line="404"/>
         <source>With Quassel IRC</source>
-        <translation type="unfinished"></translation>
+        <translation>Con Quassel IRC</translation>
     </message>
     <message>
         <location filename="../show.qml" line="438"/>
         <source>View Your Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Visualiza tus Documentos</translation>
     </message>
     <message>
         <location filename="../show.qml" line="462"/>
         <source>With QPDFView</source>
-        <translation type="unfinished"></translation>
+        <translation>Con QPDFView</translation>
     </message>
 </context>
 </TS>
