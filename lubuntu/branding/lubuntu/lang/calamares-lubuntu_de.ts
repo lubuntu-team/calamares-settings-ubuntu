82c584b4fb96988499fefe1d4221e752f25c2e2f
Daniel Absmeier <abseabsmeier@gmail.com>
2018-07-10 12:53:05 +0000

    Translated using Weblate (German)
    Currently translated at 68.7% (11 of 16 strings)
    
    Translation: Lubuntu/Calamares Slideshow
    Translate-URL: https://translate.lubuntu.me/projects/lubuntu/calamares-slideshow/de/

--- lubuntu/branding/lubuntu/lang/calamares-lubuntu_de.ts
+++ lubuntu/branding/lubuntu/lang/calamares-lubuntu_de.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name>show</name>
     <message>
         <location filename="../show.qml" line="32"/>
         <source>Get Support</source>
-        <translation type="unfinished"></translation>
+        <translation>Unterstützung erhalten</translation>
     </message>
     <message>
         <location filename="../show.qml" line="56"/>
         <source>https://lubuntu.me</source>
-        <translation type="unfinished"></translation>
+        <translation>https://lubuntu.me</translation>
     </message>
     <message>
         <location filename="../show.qml" line="90"/>
         <source>Your Mail</source>
-        <translation type="unfinished"></translation>
+        <translation>Ihre E-Mail-Adresse</translation>
     </message>
     <message>
         <location filename="../show.qml" line="114"/>
@@ -26,27 +26,27 @@
     <message>
         <location filename="../show.qml" line="148"/>
         <source>SMPlayer</source>
-        <translation type="unfinished"></translation>
+        <translation>SMPlayer</translation>
     </message>
     <message>
         <location filename="../show.qml" line="172"/>
         <source>To Play All of Your Movies</source>
-        <translation type="unfinished"></translation>
+        <translation>Um alle ihre Filme abzuspielen</translation>
     </message>
     <message>
         <location filename="../show.qml" line="206"/>
         <source>LibreOffice</source>
-        <translation type="unfinished"></translation>
+        <translation>LibreOffice</translation>
     </message>
     <message>
         <location filename="../show.qml" line="230"/>
         <source>Edits All Of Your Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Bearbeitet all ihre Dokumente</translation>
     </message>
     <message>
         <location filename="../show.qml" line="264"/>
         <source>Pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilder</translation>
     </message>
     <message>
         <location filename="../show.qml" line="288"/>
@@ -56,7 +56,7 @@
     <message>
         <location filename="../show.qml" line="322"/>
         <source>Have Fun!</source>
-        <translation type="unfinished"></translation>
+        <translation>Viel Spaß!</translation>
     </message>
     <message>
         <location filename="../show.qml" line="346"/>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../show.qml" line="380"/>
         <source>IRC Chat</source>
-        <translation type="unfinished"></translation>
+        <translation>IRC Chat</translation>
     </message>
     <message>
         <location filename="../show.qml" line="404"/>
@@ -76,7 +76,7 @@
     <message>
         <location filename="../show.qml" line="438"/>
         <source>View Your Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Ihre Dokumente anzeigen</translation>
     </message>
     <message>
         <location filename="../show.qml" line="462"/>
