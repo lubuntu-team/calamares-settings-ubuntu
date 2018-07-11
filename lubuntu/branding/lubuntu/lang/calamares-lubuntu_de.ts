6d0a95b5b6a7c5998418502f4accbe331e9f78ad
Hans P. Möller <hmoller@gmail.com>
2018-07-11 14:03:44 +0000

    Translated using Weblate (German)
    Currently translated at 87.5% (14 of 16 strings)
    
    Translation: Lubuntu/Calamares Slideshow
    Translate-URL: https://translate.lubuntu.me/projects/lubuntu/calamares-slideshow/de/

--- lubuntu/branding/lubuntu/lang/calamares-lubuntu_de.ts
+++ lubuntu/branding/lubuntu/lang/calamares-lubuntu_de.ts
@@ -61,7 +61,7 @@
     <message>
         <location filename="../show.qml" line="346"/>
         <source>With the 2048 game</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit das 2048 Spiel</translation>
     </message>
     <message>
         <location filename="../show.qml" line="380"/>
@@ -71,7 +71,7 @@
     <message>
         <location filename="../show.qml" line="404"/>
         <source>With Quassel IRC</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit Quassel IRC</translation>
     </message>
     <message>
         <location filename="../show.qml" line="438"/>
@@ -81,7 +81,7 @@
     <message>
         <location filename="../show.qml" line="462"/>
         <source>With QPDFView</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit QPDFView</translation>
     </message>
 </context>
 </TS>
